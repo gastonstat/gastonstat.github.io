d234702ad1fd0534ac9a4aa42f39ba3044218188
Gaston Sanchez <gaston.stat@gmail.com>
2014-04-19 10:43:38 -0700

    rtist gallery

--- scripts/rtist-generator.R
+++ scripts/rtist-generator.R
@@ -7,24 +7,48 @@ img_names = strsplit(images, split=".", fixed=TRUE)
 img_names = unlist(lapply(img_names, function(x) x[1]))
 
 # file name
-index = "/Users/Gaston/Documents/gastonstat.github.io/work/rtist/index.md"
+index = "/Users/Gaston/Documents/gastonstat.github.io/work/rtist/index.html"
 
-# fill file
+
+# start writing output to 'index' file
+
+# yaml header
 cat(file = index, "---", "\n")
-cat(file = index, "layout: page", "\n", append = TRUE)
+cat(file = index, "layout: flexible", "\n", append = TRUE)
 cat(file = index, "title: R-tist", "\n", append = TRUE)
 cat(file = index, "---", "\n\n", append = TRUE)
-cat(file = index, "# R-tist", "\n\n", append = TRUE)
-cat(file = index, "Some of my weird and abstract paintings with R", "\n\n",
-    append = TRUE)
 
-for (i in 1L:length(img_names)) {
+# add page title and abstract description
+cat(file = index, "<div class=\"prose\">", "\n", append = TRUE)
+cat(file = index, "  <h1>R-tist</h1>", "\n\n", append = TRUE)
+cat(file = index, "  <h4>Some of my weird and abstract paintings with R</h4>", "\n",
+    append = TRUE)
+cat(file = index, "</div>", "\n\n", append = TRUE)
+
+# populate gallery section
+cat(file = index, "<div id=\"gallery\">", "\n", append = TRUE)
+
+
+href = "  <a href=\"/work/rtist/"
+img = "  <img class=\"mosaic\" src=\"/images/rtist/"
+
+for (i in 1L:length(img_names)) 
+{
+  # add 'a' tag
   cat(file = index,
-      paste("[![", img_names[i], "]", 
-            "(/images/rtist/", images[i], ")](", 
-            "/work/rtist/", img_names[i], ".html)",
-            sep = ''),
-      "\n\n", append = TRUE)
+      paste(href, img_names[i], ".html\">", sep = ''),
+      "\n", append = TRUE)
+
+  # add 'img' element
+  cat(file = index,
+      paste(img, img_names[i], ".png\" />", sep = ''),
+      "\n", append = TRUE)
+  
+  # close 'a' tag
+  cat(file = index, "  </a>\n\n", append = TRUE)
 }
+
+# end of 'div' gallery
+cat(file = index, "</div>", "\n", append = TRUE)
 
 
