b525cec31d74f562e1b66db805f1f2ae99f67a6e
gastonstat <gaston.stat@gmail.com>
2015-10-20 08:30:13 -0700

    add post for crocodile math problem

--- blog/Rmd/knit_post_function.R
+++ blog/Rmd/knit_post_function.R
@@ -6,16 +6,37 @@ KnitPost <- function(input, base.url = "/") {
   opts_chunk$set(fig.path = fig.path)
   opts_chunk$set(fig.cap = "center")
   render_jekyll()
-  knit(input, envir = parent.frame())
+  # output file
+  output <- substr(input, 6, nchar(input))
+  output <- sub('Rmd$', 'md', output)
+  output <- paste0('_posts/', output)
+  # make md file and figs
+  input <- paste0('blog/Rmd/', input)
+  knit(input, output, envir = parent.frame())
 }
 
-setwd("/Users/Gaston/Documents/gastonstat.github.io/blog/Rmd")
+
+# i <- 0
+# repeat {
+#   x <- scan("_posts/2015-10-20-crocodile2.md", what = "", 
+#             n = 1, skip = i)
+#   print(x)
+#   if (x == "") break
+#   i <- i + 1
+# }
+
+
+#setwd("~/Documents/gastonstat.github.io/blog/Rmd")
+setwd("~/Documents/gastonstat.github.io")
 
 # remember to include yaml header lines in the produced md files
 # (they are commented out in the Rmd files to avoid jekyll crash)
 
+KnitPost("code-2015-10-20-crocodile2.Rmd")
 
-KnitPost("code-2015-02-23-splitting-rows.Rmd")
+
+#KnitPost("code-2015-10-19-crocodile-problem.Rmd")
+# KnitPost("code-2015-02-23-splitting-rows.Rmd")
 # KnitPost("code-2014-12-21-capturing-R-printed-output.Rmd")
 # KnitPost("code-2014-08-16-Density-Curves.Rmd")
 # KnitPost("code-2014-04-20-Failing-reading-table-in-R.Rmd")
